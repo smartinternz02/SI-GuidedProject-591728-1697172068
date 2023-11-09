5bf22bb849fa826c17a8946962ff923581523cac
kodipakakasthuri <129583640+kodipakakasthuri@users.noreply.github.com>
2023-11-09 08:05:05 +0530

    Add files via upload

--- py.py
+++ py.py
@@ -1,72 +1,65 @@
-import pickle
-from flask import Flask, render_template, request
-import pandas as pd 
+
+import pandas as pd
 import numpy as np
-model1=pickle.load(open('SW.pkl','rb'))
-app=Flask(__name__)
-@app.route('/')
-def home():
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+# Create flask app
+flask_app = Flask(__name__)
+model1 = pickle.load(open("SW.pkl", "rb"))
+
+@flask_app.route("/")
+def Home():
+    return render_template("index.html")
+
+@flask_app.route('/predict')
+def index():
     return render_template('index.html')
-@app.route('/predict')
-def index() :
-    return render_template('predict.html')
-@app.route('/data predict',methods=['GET','POST'])
-def predict() :
-    brand=request.form['Brand']
-    if brand=='Garmin':
-        brand=8
-    if brand=='Mobvoi':
-        brand=18
-    if brand=='Fitbit':
-        brand=6
-    if brand=='Fossil':
-        brand=7
-    if brand=='Amazfit':
-        brand=0
-    if brand=='Samsung':
-        brand=30
-    if brand=='Huawel':
-        brand=10
-    if model == 'Hybrid HR':
-        model = 44
-    if model == 'Veny Sq':
-        model = 106
-    if model == 'MagicWatch 2':
-        model = 56
-    if model == 'TicWatch Pro 3':
-        model = 97
-    if model == 'Vapor X':
-        model = 104
-    if model == 'Z':
-        model = 132
-    os = request.form['Operating System']
-    if os == 'Wear os':
-        os = 31
-    if os == 'Garmin os':
-        os = 9
-    if os == 'Lite os':
-        os = 12
-    connect = request.form['Connectivity']
-    if connect == 'Bluetooth,Wi-Fi':
-        connect = 1
-    if connect == 'Bluetooth,Wi-Fi,Cellular':
-        connect = 2
-    if connect == 'Bluetooth':
-        connect = 0
-    if connect == 'Bluetooth,Wi-Fi,GPS':
-        connect = 3
-    if connect == 'Bluetooth,Wi-Fi,NFC':
-        connect = 4
-    display_type = request.form['Display Type']
-    if display_type == 'AMOLED':
-        display_type = 0
-    if display_type == 'LED':
-        display_type = 9
-    prediction = model1.predict(pd.DataFrame([[brand,model,os,connect,display_type,display_size,resolution,water,battery,gps,nfc]],
-                                         columns= ['Brand', 'Model', 'Operating System', 'Connectivity',
-                                                    'Display Type', 'Display Size', 'Resolution',
-                                                     'Water Resistance', 'Battery Life', 'GPS','NFC']))
-    prediction = np.round(prediction,2)
-    return render_template('watch_prediction.html', prediction_text ="is {}".format(prediction))
-if __name__=='__main__':
-       flask_app.run(debug=True)
+
+@flask_app.route("/predict", methods=["POST"])
+def predict():
+    # Retrieve the form data
+    brand = request.form['Brand']
+    model = request.form['Model']
+    os = request.form.get('Operating_System', 'Default_Value')
+    connect = request.form.get('Connectivity', 'Default_Value')
+    display_type = request.form['Display_Type']
+    display_size=request.form['Display_Size_inches']
+    height = request.form['height']
+    weight=request.form['weight'] 
+    water_resistance=request.form['Water_Resistance_meters']
+    battery_life=request.form['Battery_Life_days']
+    heart_rate=request.form['Heart_Rate_Monitor']
+    gps=request.form['GPS']
+    nfc=request.form['NFC']
+    # Create a DataFrame with the form data
+    data = pd.DataFrame({
+        'Brand': [brand],
+        'Model': [model],
+        'Operating_System': [os],
+        'Connectivity': [connect],
+        'Display_Type': [display_type],
+        'Display_Size_inches':[display_size],
+        'height':[height],
+        'weight':[weight],
+        'Water_Resistance_meters':[water_resistance],
+        'Battery_Life_days':[battery_life],
+        'Heart_Rate_Monitor':[heart_rate],
+        'GPS':[gps],
+        'NFC':[nfc]
+        
+    })
+
+    # Perform one-hot encoding for categorical variables
+    data = pd.get_dummies(data, columns=['Brand', 'Model', 'Operating_System', 'Connectivity', 'Display_Type','Heart_Rate_Monitor','GPS','NFC'])
+    # Use the one-hot encoded data for prediction
+    selected_features = data.iloc[:, :6]
+    prediction = model1.predict(selected_features)
+
+    # Round the prediction to two decimal places
+    prediction = np.round(prediction, 2)
+
+    return render_template('index.html', prediction_text="The prediction is {}".format(prediction))
+
+if __name__ == '__main__':
+    flask_app.run(debug=True)
